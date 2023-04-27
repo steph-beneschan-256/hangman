66b6bcf4ecf2358206b48dd3fc109b2398609148
Steph Beneschan <36548653+steph-beneschan-256@users.noreply.github.com>
2023-04-27 00:29:02 -0700

    Update penalty counter
    * Penalty counter now uses traditional "stickman" images
    * Penalty counter now displays how many chances remain

--- src/PenaltyCounter.js
+++ src/PenaltyCounter.js
@@ -1,10 +1,34 @@
+
+
 export default function PenaltyCounter({penalties, maxPenalties}) {
     return(
         <div className="penalty-counter">
-            {/* <img src={`progress_img/${penalties}.png`} */}
-            <img src="heart.svg"
-            alt={`You've made ${penalties} mistakes so far.`}/>
-            <span>{maxPenalties + 1 - penalties}</span>
+            <div className="img-container">
+                {(penalties <= maxPenalties+1) && 
+                    <img src={`stickman/${penalties}.svg`}
+                    alt={`You've made ${penalties} mistakes so far.`}/>
+                }
+            </div>
+            <div>
+                <span className="chance-counter">
+                    {maxPenalties + 1 - penalties}
+                </span>
+                <span> {(maxPenalties + 1 - penalties === 1) ?
+                "chance remains"
+                : "chances remain"}</span>
+            </div>
+            
         </div>
     )
 }
+
+// export default function PenaltyCounter({penalties, maxPenalties}) {
+//     return(
+//         <div className="penalty-counter">
+//             {/* <img src={`progress_img/${penalties}.png`} */}
+//             <img src="heart.svg"
+//             alt={`You've made ${penalties} mistakes so far.`}/>
+//             <span>{maxPenalties + 1 - penalties}</span>
+//         </div>
+//     )
+// }
