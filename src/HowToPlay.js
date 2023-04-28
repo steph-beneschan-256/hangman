c42eb13d170a75550cc4a806d961f4594be3c9ec
Steph Beneschan <36548653+steph-beneschan-256@users.noreply.github.com>
2023-04-28 13:38:53 -0700

    Update HowToPlay.js

--- src/HowToPlay.js
+++ src/HowToPlay.js
@@ -3,23 +3,20 @@ import "./modal.css";
 export default function HowToPlay({onClose}) {
     
     return(
-        <div className="darken-screen">
-            <div className="how-to-play modal">
-                <h2>How to Play</h2>
-                <p>
-                    Every game of Hangman has a secret phrase. To win the game, you must reveal that phrase, one letter at a time. 
+        <div className="how-to-play">
+            <p>
+                Every game of Hangman has a secret phrase. To win the game, you must reveal that phrase, one letter at a time. 
+            </p>
+            <p>
+                To select a letter, either click on it using the on-screen keyboard, or (if you have a physical keyboard) type the letter on your keyboard.
                 </p>
-                <p>
-                    To select a letter, either click on it using the on-screen keyboard, or (if you have a physical keyboard) type the letter on your keyboard.
-                    </p>
-                <p>
-                    If the letter you selected is part of the secret phrase, then that part of the phrase will be revealed. Otherwise, you will lose one balloon.
-                </p>
-                <p>
-                    You begin the game with six balloons. If you lose all six, then the game is over. In order to win the game, you must fully reveal the secret phrase without losing all six baloons.
-                </p>
-                <button onClick={onClose}>Close</button>
-            </div>
+            <p>
+                If the letter you selected is part of the secret phrase, then that part of the phrase will be revealed. Otherwise, you will lose one balloon.
+            </p>
+            <p>
+                You begin the game with six balloons. If you lose all six, then the game is over. In order to win the game, you must fully reveal the secret phrase without losing all six baloons.
+            </p>
+            <button onClick={onClose}>Close</button>
         </div>
     )
 }
