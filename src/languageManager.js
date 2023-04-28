dbc88b878ab0855e31705e0bf5dfcc8584c34d10
Steph Beneschan <36548653+steph-beneschan-256@users.noreply.github.com>
2023-04-28 14:06:41 -0700

    Prevent Repeat Phrase
    * Updated the getRandomPhrase() method to prevent the user from encountering the same phrase two games in a row (until every phrase has been encountered at least once).

--- src/languageManager.js
+++ src/languageManager.js
@@ -8,6 +8,12 @@ class LanguageManager {
         this.wordSeparatorRegEx = new RegExp(data[language]["wordSeparator"]);
         this.keyboardRows = data[language]["keyboardRows"];
         this.phrases = data[language]["words"];
+        /*
+            Create an array of indices in the phrase array.
+            This array will be used to select random phrases.
+        */
+        this.wordIndices = this.phrases.map((_, i) => i);
+        console.log(this.wordIndices); 
     }
 
     isValidChar(char) {
@@ -28,8 +34,23 @@ class LanguageManager {
     }
 
     getRandomPhrase() {
-        var randomInt = Math.floor(Math.random() * this.phrases.length);
-        return this.phrases[randomInt];
+        /*
+        Of the indices of all phrases that have not yet been chosen, choose
+        an index at random. Then, replace it with the highest-numbered index.
+        This should prevent the user from encountering the same phrase two
+        games in a row, until all phrases have been encountered at least once.
+        */
+        if(this.wordIndices.length <= 0)
+            this.wordIndices = this.phrases.map((_, i) => i);
+        console.log(this.wordIndices);
+        var randomInt = Math.floor(Math.random() * this.wordIndices.length);
+        var phraseIndex = this.wordIndices[randomInt];
+
+        if(randomInt === this.wordIndices.length-1)
+            this.wordIndices.pop();
+        else
+            this.wordIndices[randomInt] = this.wordIndices.pop();
+        return this.phrases[phraseIndex];
     }
 }
 
